a740ee41d669359b1d5ac6c7163d3897fb3fdf7c
Yasir Ali <sdr.yasir@gmail.com>
2021-05-11 07:02:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+let person = {
+    name: 'Ali',
+    age: 25,
+    qual: "BS",
+    contact: '02256985478',
+    display: function () {
+        console.log('This is inside an object')
+    }
+}
+
+console.log(person.display())
