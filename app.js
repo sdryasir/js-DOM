7887b9de1fdd3bf9e50103b0be2a5159a49cfce7
Yasir Ali <sdr.yasir@gmail.com>
2021-05-11 08:57:22 +0500

    Interacting withh JS DOM - Simple example

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 
-let person = {
-    name: 'Ali',
-    age: 25,
-    qual: "BS",
-    contact: '02256985478',
-    display: function () {
-        console.log(this)
-    }
-}
+// let person = {
+//     name: 'Ali',
+//     age: 25,
+//     qual: "BS",
+//     contact: '02256985478',
+//     display: function () {
+//         console.log(this)
+//     }
+// }
+// console.log(person.display())
 
-console.log(person.display())
+
+console.log(document)
